2c31d30cf64ce711564de6dac292f5e7c2504516
Farhan Shahriar Abrar <farhan@Farhans-MacBook-Air.local>
2025-12-18 08:14:04 +0600

    added normalization and posterior re-normalization logic

--- draft-one-armed-bandit.py
+++ draft-one-armed-bandit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-
+import math
 
 # index_to_arm = {
 #     0:'centralized',
@@ -17,12 +17,14 @@ no_heirarchy = [1,1]
 
 
 
+
+
 def reward_generator():
     t = random.random()
     if t>0.5:
         return 1
     return 0
-
+#replace with actual reward generator logic or better logic to test different bandit strategies
 
 
 def generate_beta_value(arm):
@@ -30,17 +32,55 @@ def generate_beta_value(arm):
     return random_beta_value
 
 
+def normalize_beta_values(beta_values):
+    total = sum(beta_values)
+    normalized_values = [value / total for value in beta_values]
+    return normalized_values
+
+
+def posterior_normalization(current_arm, nbv, bandit_type, current_round, total_rounds):
+
+    if current_arm is None:
+        return nbv
+    
+    pbv=nbv[::]
+    if bandit_type == 'early':
+        delta = 1/(1+math.e**(current_round - total_rounds/2))
+    elif bandit_type == 'late':
+        delta = 1/(1+math.e**(total_rounds/2 - current_round))
+    else:
+        delta = 1
+
+    accumulator = 0
+
+    print(f"Delta value: {delta}")
+
+    for i in range(len(pbv)):
+        if i != current_arm:
+            accumulator += pbv[i]*(1-delta)
+            pbv[i] = pbv[i]*(delta)
+    
+    pbv[current_arm] +=  accumulator
+
+    return pbv
+
 
 def main():
 
+    total_rounds = int(input("Enter number of rounds to simulate: "))
+    arm_chosen = None
+    for i in range(total_rounds):
+        print(f"Round {i+1} \n\n")
 
-    for i in range(20):
         beta_values = [generate_beta_value(centralized), generate_beta_value(decentralized), generate_beta_value(no_heirarchy)]
-        arm_chosen = beta_values.index(max(beta_values))
+        normed_beta_values = normalize_beta_values(beta_values)
+        posterior_beta_values = posterior_normalization(arm_chosen, normed_beta_values, 'early', i, total_rounds)
+        arm_chosen = np.random.choice([0, 1, 2], p=posterior_beta_values)
 
-        print(f"Round {i+1}")
-        print(f"Centralized: {centralized}, Decentralized: {decentralized}, No Heirarchy: {no_heirarchy}")
-        print(f"Beta values - Centralized: {beta_values[0]}, Decentralized: {beta_values[1]}, No Heirarchy: {beta_values[2]}")
+        print(f"Centralized: {centralized}, Decentralized: {decentralized}, No Heirarchy: {no_heirarchy}\n")
+        print(f"Beta values - Centralized: {beta_values[0]}, Decentralized: {beta_values[1]}, No Heirarchy: {beta_values[2]}\n")
+        print(f"Normalized Beta values - Centralized: {normed_beta_values[0]}, Decentralized: {normed_beta_values[1]}, No Heirarchy: {normed_beta_values[2]}\n")
+        print(f"Posterior Beta values - Centralized: {posterior_beta_values[0]}, Decentralized: {posterior_beta_values[1]}, No Heirarchy: {posterior_beta_values[2]}\n")
         print("Arm chosen for this round:")
 
         if arm_chosen == 0:
